5766a3421ef18e8dcafda0deaee65d26496785cd
km00776 <kmurugathas@gmail.com>
2021-04-22 00:48:27 -0700

    added labels

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -8,9 +8,15 @@ import java.awt.event.ActionListener;
 public class GUI implements ActionListener  {
 
     private JLabel label;
-
+    private Worker [] workerBees = new Worker[11];
+    private Queen [] queenBees = new Queen[11];
+    private Drone [] droneBees = new Drone[11];
+    private JLabel [] queenLabels = new JLabel[queenBees.length];
+    private JLabel [] workerLabels = new JLabel[workerBees.length];
+    private JLabel [] droneLabels = new JLabel[droneBees.length];
 
     public GUI() {
+
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         JButton button = new JButton("Click me");
@@ -18,11 +24,26 @@ public class GUI implements ActionListener  {
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
-        panel.add(label);
+
+//        panel.add(label);
+
+
+        for(int i = 0; i < workerBees.length; i++) {
+           workerBees[i] = new Worker(100);
+        }
+        for(int i = 0; i < queenBees.length; i++) {
+            workerBees[i] = new Worker(100);
+        }
+        for(int i = 0; i < droneBees.length; i++) {
+            workerBees[i] = new Worker(100);
+        }
+
+
+
         frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Bee Application GUI");
-        frame.pack();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setTitle("Bee Application GUI");
+//        frame.pack();
         frame.setVisible(true);
     }
 
@@ -33,7 +54,10 @@ public class GUI implements ActionListener  {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        int random = (int) (Math.random() * 80);
+        for (int i = 0; i < workerBees.length; i++) {
+                workerBees[i].Damage((int) Math.random() * 80);
+                workerBees[i].getHealth();
+        }
 
     }
 }
